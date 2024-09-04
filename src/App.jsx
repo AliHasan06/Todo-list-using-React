3b708be25bd2ce9eed202f2baadeed1b3f6d80ab
ali hasan <alihassanraja06@gmail.com>
2024-09-04 09:13:51 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,18 @@ const App = () => {
     console.log(todoVal.current.value);
     data.push(todoVal.current.value);
     setdata([...data]);
-    todoVal.current.value =''
+    todoVal.current.value = "";
   };
-  const deleteTodo =(index)=>{
-    console.log('delete todo', index  );
-    data.splice(index , 1)
-    setdata([...data])
-  }
-  const editTodo =(index,value)=>{
-    console.log('edit todo' , value);
-    data.splice(index , 1, value)
-    setdata([...data])
-    
-  }
+  const deleteTodo = (index) => {
+    console.log("delete todo", index);
+    data.splice(index, 1);
+    setdata([...data]);
+  };
+  const editTodo = (index, value) => {
+    console.log("edit todo", value);
+    data.splice(index, 1, value);
+    setdata([...data]);
+  };
   return (
     <>
       <h1>todo list</h1>
@@ -30,11 +29,19 @@ const App = () => {
         <input type="text" ref={todoVal} placeholder="enter todo" />
         <button type="submit">submit</button>
       </form>
-      {data.length > 0 ? 
-        data.map((item , index)=>{
-          return <Card key={index} title={item} deleteTodo={deleteTodo} editTodo={editTodo} index={index} />
+      {data.length > 0 ? (
+        data.map((item, index) => {
+          return (
+            <Card
+              key={index}
+              title={item}
+              deleteTodo={deleteTodo}
+              editTodo={editTodo}
+              index={index}
+            />
+          );
         })
-      : (
+      ) : (
         <h1>No data found....</h1>
       )}
     </>
